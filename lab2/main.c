38c581c8be8262d02d38d24ff05c7b5887396aca
Shunskiy <pas4o@yandex.ru>
2021-01-19 17:49:24 +0400

    Add files via upload

--- lab2/main.c
+++ lab2/main.c
@@ -3,11 +3,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+//Функция отключения всех диодов
+void disableAll {
+    PORTE &= ~(1 << PE1);
+    PORTE &= ~(1 << PE2);
+    PORTE &= ~(1 << PE3);
+}
+
 int main ()
 {
-    DDRE |= (1 << PE0);
+    DDRE |= (1 << PE1);
+    DDRE |= (1 << PE2);
+    DDRE |= (1 << PE3);
+    DDRE &= ~(1 << PE4);
     DDRE &= ~(1 << PE5);
-    PORTE &= ~(1 << PE0);
+    PORTE |= (1 << PE1); //1 диод горит сразу
+    PORTE &= ~(1 << PE2);
+    PORTE &= ~(1 << PE3);
+    PORTE |= (1 << PE4);
     PORTE |= (1 << PE5);
     
     uint8_t btn_click_counter = 0;
@@ -15,12 +28,19 @@ int main ()
     
     while (1)
     {
-        if ((PINE & (1 << PE5)) == 0)
+        //Кнопка перехода вперёд
+        if ((PINE & (1 << PE4)) == 0)
         {
             _delay_ms(10);
             if (btn_ticks_counter == 0)
             {
-                btn_click_counter++;
+                //Проверка последнего диода
+                if (btn_click_counter == 2) {
+                    btn_click_counter = 0;
+                } else {
+                    btn_click_counter++;    
+                }
+                
             }
             
             if (btn_ticks_counter < 10){
@@ -31,15 +51,40 @@ int main ()
             btn_ticks_counter = 0;
         }
         
-        if (btn_ticks_counter == 9){
-            for (int i = 1; i < btn_click_counter; i++) {
-				PORTE |= (1 << PE0);
-				_delay_ms(100);
-				PORTE &= ~(1 << PE0);
-				_delay_ms(100);
-			}
-			
-			btn_click_counter = 0;
+        
+        //Кнопка перехода назад
+        if ((PINE & (1 << PE5)) == 0)
+        {
+            _delay_ms(10);
+            if (btn_ticks_counter == 0)
+            {
+                //Проверка первого диода
+                if (btn_click_counter == 0) {
+                    btn_click_counter = 2;
+                } else {
+                    btn_click_counter--;    
+                }
+            }
+            
+            if (btn_ticks_counter < 10){
+                btn_ticks_counter++;
+            }
+        } else
+        {
+            btn_ticks_counter = 0;
         }
+        
+        
+        
+        //Переключение диода взависимости от значения btn_click_counter
+        if (btn_click_counter == 0){
+                disableAll();
+                PORTE &= ~(1 << PE1);
+		} else if (btn_click_counter == 1 ){
+                disableAll();
+                PORTE &= ~(1 << PE2);
+		} else if (btn_click_counter == 2 ){
+                disableAll();
+                PORTE &= ~(1 << PE3);
     }
 }
